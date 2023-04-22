e647d510ad2fb7beaf2f1aec5194e55aa56a14a9
Federica Ventura <federica.ventura@aesys.tech>
2023-04-22 18:55:07 +0200

    code optimization

--- counter.js
+++ counter.js
@@ -1,17 +1,35 @@
 let counter = 0;
-const counterElement = document.querySelector("#counter-value");
+// const counterElement = document.querySelector("#counter-value");
 
-function increaseCounter() {
-  counter += 1;
-  counterElement.innerText = `${counter}`;
-}
+// function increaseCounter() {
+//   counter += 1;
+//   counterElement.innerText = `${counter}`;
+// }
 
-function decreaseCounter() {
-  counter -= 1;
-  counterElement.innerText = `${counter}`;
-}
+// function decreaseCounter() {
+//   counter -= 1;
+//   counterElement.innerText = `${counter}`;
+// }
 
-function resetCounter() {
-  counter = 0;
+// function resetCounter() {
+//   counter = 0;
+//   counterElement.innerText = `${counter}`;
+// }
+
+function handleCounter(operator) {
+  const counterElement = document.querySelector("#counter-value");
+  switch (operator) {
+    case "increase":
+      counter += 1;
+      break;
+    case "decrease":
+      counter -= 1;
+      break;
+    case "reset":
+      counter = 0;
+      break;
+    default:
+      return counter;
+  }
   counterElement.innerText = `${counter}`;
 }
