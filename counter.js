d4ffd319bbaf72ce6917c4cf463e4196e8a65b08
Federica Ventura <federica.ventura@aesys.tech>
2023-04-24 14:52:35 +0200

    add css rules

--- counter.js
+++ counter.js
@@ -1,23 +1,8 @@
 let counter = 0;
-// const counterElement = document.querySelector("#counter-value");
-
-// function increaseCounter() {
-//   counter += 1;
-//   counterElement.innerText = `${counter}`;
-// }
-
-// function decreaseCounter() {
-//   counter -= 1;
-//   counterElement.innerText = `${counter}`;
-// }
-
-// function resetCounter() {
-//   counter = 0;
-//   counterElement.innerText = `${counter}`;
-// }
 
 function handleCounter(operator) {
   const counterElement = document.querySelector("#counter-value");
+  
   switch (operator) {
     case "increase":
       counter += 1;
