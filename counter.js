56fa4e12b5a30964b28755a0bdf8a7907f2753c3
Federica Ventura <federica.ventura@aesys.tech>
2023-04-24 15:09:58 +0200

    change button position and counter logic

--- counter.js
+++ counter.js
@@ -2,13 +2,15 @@ let counter = 0;
 
 function handleCounter(operator) {
   const counterElement = document.querySelector("#counter-value");
-  
+
   switch (operator) {
     case "increase":
       counter += 1;
       break;
     case "decrease":
-      counter -= 1;
+      if (counter > 0) {
+        counter -= 1;
+      } else counter;
       break;
     case "reset":
       counter = 0;
